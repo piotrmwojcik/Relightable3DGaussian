58dfd4c3efd04bbf04285f9d8258d59b84ced37f
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-01 12:31:53 +0200

    all datasets

--- scene/__init__.py
+++ scene/__init__.py
@@ -54,7 +54,7 @@ class Scene:
                                                             debug=args.debug_cuda)
             else:
                 print("Found transforms_train.json file, assuming Blender data set!")
-                print(args)
+                print(print(vars(args)))
                 scene_info = sceneLoadTypeCallbacks["Blender"](args.source_path, args.white_background, args.envmap_prefix,
                                                                args.eval, debug=args.debug_cuda)
         elif os.path.exists(os.path.join(args.source_path, "inputs/sfm_scene.json")):
