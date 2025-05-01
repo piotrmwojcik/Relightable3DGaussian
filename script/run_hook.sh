ff3b69d589b0b64d782c22eab27d3609da82a07a
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-01 11:19:49 +0200

    all datasets

--- script/run_hook.sh
+++ script/run_hook.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 root_dir="datasets/nerf_synthetic/"
+object="hook100_statictimestep50"
 list="chapel_day_4k_32x16_rot0 dam_wall_4k_32x16_rot90 golden_bay_4k_32x16_rot330"
 
 for i in $list; do
     python train.py --eval \
-        -s datasets/nerf_synthetic/hook100_statictimestep50/ \
+        -s datasets/nerf_synthetic/$object/ \
         -m output/our_ds/$i/3dgs \
         --lambda_normal_render_depth 0.01 \
         --lambda_normal_smooth 0.01 \
@@ -19,7 +20,7 @@ for i in $list; do
         -c output/our_ds/${i}/3dgs/chkpnt30000.pth
 
     python train.py --eval \
-        -s datasets/nerf_synthetic/hook100_statictimestep50/ \
+        -s datasets/nerf_synthetic/$object/ \
         -m output/our_ds/$i/neilf \
         -c output/our_ds/$i/3dgs/chkpnt30000.pth \
         --save_training_vis \
